cfeafe81205936bdbcbe3cf7a0f4bf64ec04f280
aralukene <59538075+aralukene@users.noreply.github.com>
2023-10-02 09:14:07 +0100

    Updated

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True;
